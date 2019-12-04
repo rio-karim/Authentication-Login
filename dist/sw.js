5025610a71912c3fbc262953fe9c1187ed67f698
Rio Karim <rio.karim@gmail.com>
2019-12-04 08:42:20 +0000

    <Dist> generated

--- dist/sw.js
+++ dist/sw.js
@@ -2,40 +2,40 @@ importScripts('/_nuxt/workbox.4c4f5ca6.js')
 
 workbox.precaching.precacheAndRoute([
   {
-    "url": "/_nuxt/153b559ae935100199e5.js",
-    "revision": "f9a4c21f26f0c966b4f4d957e0195e86"
+    "url": "/_nuxt/3499467fed6f9d46ac6d.js",
+    "revision": "53350c507a5a9aafeacfb8cf768d00b2"
   },
   {
-    "url": "/_nuxt/23b1fb08ff8fafcd1c9a.js",
-    "revision": "1a4cf709088b41310c1570ddde8c41cf"
+    "url": "/_nuxt/3805cad7f93d5c6faed6.js",
+    "revision": "cb6612ba525df2e9131867b055b69246"
   },
   {
-    "url": "/_nuxt/2b5142369dce7a459517.js",
-    "revision": "56eb20bb1c7d4879da1901099cd647e5"
+    "url": "/_nuxt/423964c0996c7c32c5b6.js",
+    "revision": "e6dd993f8c147e1a0cedd67a443f20e3"
   },
   {
-    "url": "/_nuxt/382b4764046ef038d70c.js",
-    "revision": "d35f4a7965782c3392fe3e93e020bd86"
+    "url": "/_nuxt/4a7dd6fe67bc771785f1.js",
+    "revision": "745c3e3369acb120a6a2dffd87d4619d"
   },
   {
-    "url": "/_nuxt/8bc616727d0dc47d92ab.js",
-    "revision": "117d22492bcddd3dac03e6793ad814c0"
+    "url": "/_nuxt/5ce48e2573d16ee34fa1.js",
+    "revision": "ddd8c11e81b3e21be6176d29292f71e5"
   },
   {
-    "url": "/_nuxt/a24dcaf84a9cc7808e98.js",
-    "revision": "d548e1a2a2e24fcc67d37af6bf6682f5"
+    "url": "/_nuxt/8473c20c30277062ff9c.js",
+    "revision": "d6d8796ed6642a21ee71c962095fbb64"
   },
   {
-    "url": "/_nuxt/d585883749e8d6de51a1.js",
-    "revision": "3f16a01ccb0c6aee322ec0ee748b9afc"
+    "url": "/_nuxt/cdc88f2765a7f9e0478d.js",
+    "revision": "5e2e0fa52afee678466de10f1095976a"
   },
   {
-    "url": "/_nuxt/d6ccf415a815da0c18aa.js",
-    "revision": "344afb5c612dad98fd457d1233cee65c"
+    "url": "/_nuxt/d5ed8a5c19202a10b4d4.js",
+    "revision": "d7b8916e31619c0ee8422130bc2fc7d2"
   },
   {
-    "url": "/_nuxt/d8a98be97f5f1cbccede.js",
-    "revision": "62ceaf721a4927f71b0861e0fc667fb7"
+    "url": "/_nuxt/e411987ba2326c41cfef.js",
+    "revision": "5598012c953600c4f0421eae1b13a326"
   }
 ], {
   "cacheId": "ops-ui",
